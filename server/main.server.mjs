6b3c880a9b43c1c9c74f677d6ce9a800eb788110
satishreddimedapati <satishreddimedapati@gmail.com>
2024-09-09 13:54:08 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-I4HBKBU2.mjs";import"./chunk-X3HKTA53.mjs";import"./chunk-SHV6AYJC.mjs";import"./chunk-3AYTSRYC.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
